cdb91c245e367d60088591b72b71f16c47e83120
Aposika <apule.osika12@gmail.com>
2023-05-03 12:30:50 -0400

    Added script to all html pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
 console.log("Hi! Welcome to my portfolio site. Take a look around!");
+var escapedText1 = 'He said: "Yes!", that\'s for sure';
+var escapedText2 = 'He said: "Yes!", that\'s for sure';
+console.log(text1, text2);
+console.log(thisVariableDoesntExist);
+var emptyVariable = null;
+console.log(doesntExist);
+console.log(emptyVariable);
