b22d83f027f7bf4f89bed8cb0a8a23b14367060a
Aposika <apule.osika12@gmail.com>
2023-05-15 20:15:59 -0400

    Changed padding

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,10 @@
 console.log("Hi! Welcome to my portfolio site. Take a look around!");
-var escapedText1 = 'He said: "Yes!", that\'s for sure';
-var escapedText2 = 'He said: "Yes!", that\'s for sure';
-console.log(text1, text2);
-console.log(thisVariableDoesntExist);
-var emptyVariable = null;
-console.log(doesntExist);
-console.log(emptyVariable);
 
-<script
-  src="https://code.jquery.com/jquery-3.6.4.min.js"
-  integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8="
-  crossorigin="anonymous"
-></script>;
+function menuToggle() {
+  var x = document.getElementById("myNavtoggle");
+  if (x.className === "navtoggle") {
+    x.className += " responsive";
+  } else {
+    x.className = "navtoggle";
+  }
+}
